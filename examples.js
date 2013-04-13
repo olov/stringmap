63675e70fd1e6a946b24659b5db9c8ad33d2891a
Olov Lassus <olov.lassus@gmail.com>
2013-04-13 23:24:41 +0200

    examples

--- examples.js
+++ examples.js
@@ -1,14 +1,3 @@
-# stringmap.js
-A fast and robust stringmap implementation that can hold any string keys,
-including `__proto__`, with minimal overhead compared to a plain object.
-Works in node and browsers.
-
-
-
-## Examples
-Available in `examples.js`
-
-```javascript
 var StringMap = require("./stringmap");
 
 var sm1 = new StringMap();
@@ -43,26 +32,3 @@ sm3.setMany({
     b: [],
 });
 console.log(sm3.toString()); // {"check":true,"one":1,"two":2,"a":{},"b":[],"__proto__":-1}
-```
-
-
-
-## Installation
-
-### Node
-Install using npm
-
-    npm install stringmap
-
-```javascript
-var StringMap = require("stringmap");
-```
-
-### Browser
-Clone the repo, include it in a script tag. You may want an ES5 polyfill.
-
-    git clone https://github.com/olov/stringmap.git
-
-```html
-<script src="stringmap/stringmap.js"></script>
-```
